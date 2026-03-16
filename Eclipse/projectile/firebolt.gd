460803d2ccaa069c9590cf9fbb18adb01ac93a4b
cheshirefella <rogalskyn@gmail.com>
2026-03-16 11:33:07 -0500

    Create mage and firebolt scenes (#8)
    * Create ~libgit_plugin.windows.editor.x86_64.dll
    
    idk what this does
    
    * Revert "Create ~libgit_plugin.windows.editor.x86_64.dll"
    
    This reverts commit e6cecdca3ae79385fbf146f578685879a3bfea78.
    
    * create mage and firebolt scenes
    
    * simplify los calculation
    
    Co-authored-by: Jordon Hong <57729746+JordonH03@users.noreply.github.com>
    
    * small fixes to mage.gd and firebolt.gd
    
    * Update mage.gd
    
    ---------
    
    Co-authored-by: Jordon Hong <57729746+JordonH03@users.noreply.github.com>

---
+++ Eclipse/projectile/firebolt.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 200
+
+var direction : Vector2
+var spawnPos : Vector2
+var spawnRot : float  # Used to orient a sprite with a head/tail.
+
+signal hit_player
+
+
+func _ready():
+	global_position = spawnPos
+	global_rotation = spawnRot
+
+
+func _physics_process(delta):
+	velocity = direction * SPEED
+	move_and_slide()
+	HandleCollisions()
+
+
+func HandleCollisions():
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+	
+		if collision.get_collider() == null:
+			continue
+		
+		if collision.get_collider().is_in_group("slimy"):  # TODO: set to minion group
+			hit_player.emit()
+	
+		queue_free()
+
+
+# Delete the firebolt object after it has traveled a set time.
+func _on_expire_timeout():
+	queue_free()
