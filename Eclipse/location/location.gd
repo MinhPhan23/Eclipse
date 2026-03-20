e4daef7b1b2996b3ae2c887a7cc03e085cb3b2d8
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2026-03-20 15:03:33 -0500

    feat: Trigger end battle on entity death

--- Eclipse/location/location.gd
+++ Eclipse/location/location.gd
@@ -6,8 +6,10 @@ extends Area2D
 
 @export var events: Array[String]
 @export var pattern_index: int
-@export var minion_bonus = 2
-@export var battle_trigger_range = 3
+@export var minion_bonus: int = 2
+@export var battle_trigger_range: int = 3
+@export var minion_spawn_pos: Vector2 = Vector2(30, 30)
+@export var hero_spawn_pos: Vector2 = Vector2(50, 50)
 
 var hero: CharacterBody2D
 var minion: CharacterBody2D
@@ -50,6 +52,7 @@ func hero_remove():
 	
 func minion_add(new_minion):
 	minion = new_minion
+	hero.TARGET = new_minion
 	
 func move_minion(new_location):
 	if (new_location.minion == null):
@@ -57,6 +60,7 @@ func move_minion(new_location):
 	minion = null
 	
 func minion_remove():
+	hero.TARGET = null
 	minion.queue_free()
 	minion = null
 	
--- Eclipse/main.gd
+++ Eclipse/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var next_day : PanelContainer
 
 @onready var minion_scene_preload = preload("res://minion/minion.tscn")
-@onready var hero_scene_preload = preload("res://minion/minion.tscn")
+@onready var hero_scene_preload = preload("res://mage/mage.tscn")
 @onready var location_array = [$"Castle"]
 
 # Called when the node enters the scene tree for the first time.
@@ -11,8 +11,9 @@ func _ready():
 	next_day.choices = ["Next day"]
 	next_day.label_text = ""
 	
-	location_array[0].hero = hero_scene_preload.instantiate()
-	location_array[0].minion = minion_scene_preload.instantiate()
+	for location in location_array:
+		location.hero = hero_scene_preload.instantiate()
+		location.minion = minion_scene_preload.instantiate()
 
-func _on_next_day_selected(index):
+func _on_next_day_selected(_index):
 	location_array[0]._open_battle_confirmation_dialog()
