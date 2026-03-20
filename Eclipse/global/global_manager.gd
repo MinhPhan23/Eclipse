9f2254dc8ae2464209c189153653deea49685044
Noor <shamiln@myumanitoba.ca>
2026-03-20 16:28:10 -0500

    removed set_minion_choice

--- Eclipse/global/global_manager.gd
+++ Eclipse/global/global_manager.gd
@@ -6,8 +6,8 @@ extends Node
 
 var location: Array[Node];
 var minion_list: Array[Node];
-var rand_num:RandomNumberGenerator; 
-var curr_loc:String;
+var rand_num: RandomNumberGenerator;
+var curr_loc: String;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rand_num = RandomNumberGenerator.new()
@@ -25,27 +25,23 @@ func _ready():
 	
 	
 func send_minion_dialog(location):
-	set_minion_choice()
+	minion_choice.choices = minion_arr()
 	minion_choice.visible = true
 	minion_choice.process_mode = Node.PROCESS_MODE_INHERIT
 	curr_loc = location
-
-func set_minion_choice():
-	minion_choice.choices = minion_arr()
 	
 func minion_arr() -> Array[String]:
-	var arr:Array[String] = []
+	var arr: Array[String] = []
 	for i in 3:
-		arr.append("Minion %d Atk:%d Lvl:%d Health:%d/%d"%[i + 1 ,minion_list[i].strength,minion_list[i].level, minion_list[i].current_health,minion_list[i].MAX_HEALTH])
+		arr.append("Minion %d Atk:%d Lvl:%d Health:%d/%d" % [i + 1, minion_list[i].strength, minion_list[i].level, minion_list[i].current_health, minion_list[i].MAX_HEALTH])
 	return arr
 
 func generate_hero():
-	var index:int = rand_num.randi_range(0, location.size() - 1)
+	var index: int = rand_num.randi_range(0, location.size() - 1)
 	if location[index].hero == null:
 		location[index].hero_add(hero_scene_preload.instantiate())
 	
 func send_minion(index):
-
 	minion_choice.visible = false
 	minion_choice.process_mode = Node.PROCESS_MODE_DISABLED
 	for i in location:
@@ -54,7 +50,7 @@ func send_minion(index):
 	
 
 func generate_event():
-	var index:int = rand_num.randi_range(0, location.size() - 1)
+	var index: int = rand_num.randi_range(0, location.size() - 1)
 	for i in location:
 		if i.has_method("generate_events"):
 			i.generate_events()
@@ -62,5 +58,3 @@ func generate_event():
 func update_game_world():
 	generate_hero()
 	generate_event()
-	
-	
