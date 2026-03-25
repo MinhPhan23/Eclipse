41f098092817805c3efe3e5ace36470c16a21483
JordonH03 <jordonhong03@gmail.com>
2026-03-24 21:44:35 -0500

    feat: Add eclipse countdown to main UI

--- Eclipse/main.gd
+++ Eclipse/main.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-@export var next_day : PanelContainer
+@export var next_day: PanelContainer
 
-@onready var minion_scene_preload = preload("res://minion/minion.tscn")
-@onready var hero_scene_preload = preload("res://mage/mage.tscn")
-@onready var location_manager : Node2D = $"LocationManager"
-@onready var report : Control = $CanvasLayer/Report
+@onready var minion_scene_preload: PackedScene = preload("res://minion/minion.tscn")
+@onready var hero_scene_preload: PackedScene = preload("res://mage/mage.tscn")
+@onready var location_manager: Node2D = $"LocationManager"
+@onready var report: Control = $UI/Report
+@onready var countdown: Node2D = $UI/Countdown
 
 var day: int
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +18,8 @@ func proceed_to_next_day(_index):
 	day += 1
 	location_manager.update_game_world()
 	var report_str: String = _generate_report_string()
+	
+	countdown.next_day()
 	
 	report.show_report(report_str)
 
