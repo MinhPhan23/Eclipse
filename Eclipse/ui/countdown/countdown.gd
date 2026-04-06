6ea69e2464c78fe93c0e4b16fddece01adf71fee
Minh Phan <94029668+MinhPhan23@users.noreply.github.com>
2026-04-06 13:10:16 -0500

    - Put report to highest layer (#75)
    - Fix countdown out of bound error

--- Eclipse/ui/countdown/countdown.gd
+++ Eclipse/ui/countdown/countdown.gd
@@ -3,8 +3,11 @@ extends Control
 @onready var sprite: Sprite2D = $EclipseSprite
 @onready var label: Label = $Label
 
+func _ready():
+	sprite.frame = 0
+
 func _set_ui() -> void:
-	sprite.frame = min(Globals.current_day / 2 - 1, sprite.hframes - 1)
+	sprite.frame = max(Globals.current_day / 2 - 1, 0)
 	label.text = "Day %d" % (Globals.current_day)
 
 func next_day() -> void:
